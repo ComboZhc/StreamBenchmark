db60baf7aedcea2e7a8c336f2a3ee84d095d1507
Chao Zhang <zhangchao6865@gmail.com>
2015-10-19 15:59:43 -0400

    Raw json tweets

--- load-generator/src/main/java/chao/cmu/capstone/LoadGenerator.java
+++ load-generator/src/main/java/chao/cmu/capstone/LoadGenerator.java
@@ -1,6 +1,5 @@
 package chao.cmu.capstone;
 
-import com.google.common.collect.Lists;
 import com.twitter.hbc.ClientBuilder;
 import com.twitter.hbc.core.Constants;
 import com.twitter.hbc.core.endpoint.StatusesSampleEndpoint;
@@ -8,19 +7,9 @@ import com.twitter.hbc.core.processor.StringDelimitedProcessor;
 import com.twitter.hbc.httpclient.BasicClient;
 import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
-import com.twitter.hbc.twitter4j.Twitter4jStatusClient;
-import com.twitter.hbc.twitter4j.handler.StatusStreamHandler;
-import com.twitter.hbc.twitter4j.message.DisconnectMessage;
-import com.twitter.hbc.twitter4j.message.StallWarningMessage;
 import org.apache.kafka.clients.producer.*;
-import org.apache.kafka.common.PartitionInfo;
-import twitter4j.StallWarning;
-import twitter4j.Status;
-import twitter4j.StatusDeletionNotice;
-import twitter4j.StatusListener;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.*;
 
@@ -29,54 +18,14 @@ public class LoadGenerator {
     private static String CONSUMER_SECRET = "yfb5vkZalapbYJEYCiQJRKgtW3NY2FWYiSnvCUiPibuHTkKtFa";
     private static String ACCESS_TOKEN = "287477012-X8NS8oXM5ojzWQiDQHTDRTfL9a78tZIGn3P0qoeI";
     private static String ACCESS_SECRET = "8h9bKnxqqsL3NbTP11fIIBQnmf4ewUbGzash96QdOj3Bz";
-    private static KafkaProducer<String, String> producer;
-    private static StatusListener listener = new StatusStreamHandler() {
-        @Override
-        public void onStatus(Status status) {
-            System.out.println(status.getId());
-            try {
-                producer.send(new ProducerRecord<String, String>("test", status.toString())).get();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (ExecutionException e) {
-                e.printStackTrace();
-            }
-        }
 
-        @Override
-        public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {}
-
-        @Override
-        public void onTrackLimitationNotice(int limit) {}
-
-        @Override
-        public void onScrubGeo(long user, long upToStatus) {}
-
-        @Override
-        public void onStallWarning(StallWarning warning) {}
-
-        @Override
-        public void onException(Exception e) {}
-
-        @Override
-        public void onDisconnectMessage(DisconnectMessage message) {}
-
-        @Override
-        public void onStallWarningMessage(StallWarningMessage warning) {}
-
-        @Override
-        public void onUnknownMessageType(String s) {}
-    };
-
-    public static void main(String[] args) throws InterruptedException {
-
-
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         Map<String, Object> configs = new HashMap<String, Object>();
         configs.put("bootstrap.servers", args[0]);
         configs.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         configs.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         configs.put("client.id", "load-generator");
-        producer = new KafkaProducer<String, String>(configs);
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(configs);
 
         BlockingQueue<String> queue = new LinkedBlockingQueue<String>(10000);
         StatusesSampleEndpoint endpoint = new StatusesSampleEndpoint();
@@ -87,10 +36,10 @@ public class LoadGenerator {
                 .authentication(auth)
                 .processor(new StringDelimitedProcessor(queue))
                 .build();
-        ExecutorService service = Executors.newFixedThreadPool(2);
-        Twitter4jStatusClient t4jClient = new Twitter4jStatusClient(
-                client, queue, Lists.newArrayList(listener), service);
-        t4jClient.connect();
-        t4jClient.process();
+        client.connect();
+        while (!client.isDone()) {
+            String msg = queue.take();
+            producer.send(new ProducerRecord<String, String>("test", msg)).get();
+        }
     }
 }
