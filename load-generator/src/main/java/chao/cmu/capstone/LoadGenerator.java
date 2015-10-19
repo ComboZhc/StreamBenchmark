22afa9388a7235d0b076d16b7667483b55c25a4d
Chao Zhang <zhangchao6865@gmail.com>
2015-10-19 14:08:34 -0400

    Update

--- load-generator/src/main/java/chao/cmu/capstone/LoadGenerator.java
+++ load-generator/src/main/java/chao/cmu/capstone/LoadGenerator.java
@@ -1,3 +1,5 @@
+package chao.cmu.capstone;
+
 import com.google.common.collect.Lists;
 import com.twitter.hbc.ClientBuilder;
 import com.twitter.hbc.core.Constants;
@@ -10,25 +12,35 @@ import com.twitter.hbc.twitter4j.Twitter4jStatusClient;
 import com.twitter.hbc.twitter4j.handler.StatusStreamHandler;
 import com.twitter.hbc.twitter4j.message.DisconnectMessage;
 import com.twitter.hbc.twitter4j.message.StallWarningMessage;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.PartitionInfo;
 import twitter4j.StallWarning;
 import twitter4j.Status;
 import twitter4j.StatusDeletionNotice;
 import twitter4j.StatusListener;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.*;
 
 public class LoadGenerator {
     private static String CONSUMER_KEY = "3y2XFYQtooHvTrqX64a4VIhiu";
     private static String CONSUMER_SECRET = "yfb5vkZalapbYJEYCiQJRKgtW3NY2FWYiSnvCUiPibuHTkKtFa";
     private static String ACCESS_TOKEN = "287477012-X8NS8oXM5ojzWQiDQHTDRTfL9a78tZIGn3P0qoeI";
     private static String ACCESS_SECRET = "8h9bKnxqqsL3NbTP11fIIBQnmf4ewUbGzash96QdOj3Bz";
+    private static KafkaProducer<String, String> producer;
     private static StatusListener listener = new StatusStreamHandler() {
         @Override
         public void onStatus(Status status) {
-            System.out.println(status.getCreatedAt().toString());
+            System.out.println(status.getId());
+            try {
+                producer.send(new ProducerRecord<String, String>("test", status.toString())).get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
         }
 
         @Override
@@ -58,33 +70,26 @@ public class LoadGenerator {
 
     public static void main(String[] args) throws InterruptedException {
 
+
+        Map<String, Object> configs = new HashMap<String, Object>();
+        configs.put("bootstrap.servers", args[0]);
+        configs.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        configs.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        configs.put("client.id", "load-generator");
+        producer = new KafkaProducer<String, String>(configs);
+
         BlockingQueue<String> queue = new LinkedBlockingQueue<String>(10000);
-
-        // Define our endpoint: By default, delimited=length is set (we need this for our processor)
-        // and stall warnings are on.
         StatusesSampleEndpoint endpoint = new StatusesSampleEndpoint();
-
         Authentication auth = new OAuth1(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET);
-        // Authentication auth = new BasicAuth(username, password);
-
-        // Create a new BasicClient. By default gzip is enabled.
         BasicClient client = new ClientBuilder()
                 .hosts(Constants.STREAM_HOST)
                 .endpoint(endpoint)
                 .authentication(auth)
                 .processor(new StringDelimitedProcessor(queue))
                 .build();
-
-        // Create an executor service which will spawn threads to do the actual work of parsing the incoming messages and
-        // calling the listeners on each message
-        int numProcessingThreads = 2;
-        ExecutorService service = Executors.newFixedThreadPool(numProcessingThreads);
-
-        // Wrap our BasicClient with the twitter4j client
+        ExecutorService service = Executors.newFixedThreadPool(2);
         Twitter4jStatusClient t4jClient = new Twitter4jStatusClient(
                 client, queue, Lists.newArrayList(listener), service);
-
-        // Establish a connection
         t4jClient.connect();
         t4jClient.process();
     }
